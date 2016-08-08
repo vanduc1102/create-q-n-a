afddd65e46d36fd11e53df3f706574a1fb08e1d6
Duc Nguyen <vanducld@gmail.com>
2016-08-09 00:16:16 +0700

    basic functions

--- create-q-n-a/index.php
+++ create-q-n-a/index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
-<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 <head>
 	<title>Creating question and anwser</title>
-	<script type="text/javascript" src="main.js"></script>
 	<link rel="stylesheet" type="text/css" href="style.css" />
+	<script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+	<!-- <script type="text/javascript" src="./ckeditor/ckeditor.js"></script> -->
+	<script src="//cdn.ckeditor.com/4.5.10/standard/ckeditor.js"></script>
+	<script type="text/javascript" src="main.js"></script>
+
 </head>
 <body>
 <?php
@@ -20,7 +23,6 @@
 			<?php createCagetorySelectorElement($dbConnection, "question-category"); ?>
 			<div class=" question-composer-container">
 				<div class="question-composer" id="question" contenteditable="true"></div>
-				<div class='placeholder'>paste HTML here <br><br>Go to some website and select things with your mouse and copy-paste it here</div>
 			</div>
 			<input class="width-90" id="question-tags" placeholder="tags" />
 			<!-- <input placeholder="Choose user ask"/> -->
@@ -30,25 +32,11 @@
 			<h1>Answer : </h1>
 			<div class="anwser-composer-container">
 				<div class="anwser-composer" id="answer" contenteditable="true"></div>
-				<div class='placeholder'>paste HTML here <br><br>Go to some website and select things with your mouse and copy-paste it here</div>
 			</div>
 			<!-- <input placeholder="Choose user answer"> -->
 			<?php createUserSelectorElement($dbConnection, "user-answer"); ?>
 		</div>
 		<button class="btn btn-primary" onclick="onSubmitData(event)">Submit</button>
-	</div>
-	<div class="preview col-xs-12 col-lg-6">
-		<div class="container row">
-			<h1>Preview here :</h1>
-			<h2>Question : </h2>
-			<div class="row" id="question-preview">
-				
-			</div>
-			<h2>Answer : </h2>
-			<div class="row" id="answer-preview">
-				
-			</div>
-		</div>
 	</div>
 </div>
 
--- create-q-n-a/main.js
+++ create-q-n-a/main.js
@@ -18,12 +18,12 @@ function onSubmitData($event) {
 function getData() {
     var questionTitle = $("#question-title").val();
     var questionCategory = $("#question-category").val();
-    var questionBody = $("#question").html();
+    var questionBody = CKEDITOR.instances.question.getData();
     var questionUser = $("#user-ask").val();
     var questionTags = $("#question-tags").val();
 
     var answerUser = $("#user-answer").val();
-    var answerBody = $("#answer").html();
+    var answerBody = CKEDITOR.instances.answer.getData();
     return {
         'q-title': questionTitle,
         'q-user': questionUser,
@@ -35,53 +35,10 @@ function getData() {
     };
 }
 
+CKEDITOR.config.height = 150;
+CKEDITOR.config.width = 'auto';
 
-
-/////// EVENT BINDING /////////
-
-// jQuery delegated event listener
-$(document).on('paste', '#question', onPasteQuestion);
-$(document).on('paste', '#answer', onPasteAnswer);
-//$(document).on('keyup', '#question', onKeyUpQuestion);
-//$(document).on('keyup', '#answer', onKeyUpAnswer);
-// on-element event listener
-// document.body.addEventListener("paste", onPaste);
-function onKeyUpQuestion(e){
-  e.preventDefault();
-  copyDataToReview("#question","#question-preview");
-}
-function onKeyUpAnswer(e){
-  e.preventDefault();
-  copyDataToReview("#answer", "#answer-preview");
-}
-
-function copyDataToReview($origin,$preview){
-  var originBody = $($origin).html();
-  $($preview).html(originBody);
-}
-function onPasteQuestion(e) {
-    var element = "#question-preview";
-    pasteData(e, element);
-}
-
-function onPasteAnswer(e) {
-    var element = "#answer-preview";
-    pasteData(e, element);
-}
-
-function pasteData(e, element) {
-    var content;
-    e.preventDefault();
-    if (e.originalEvent.clipboardData) {
-        content = (e.originalEvent || e).clipboardData.getData('text/html');
-        $(element).html(content);
-        document.execCommand('insertText', false, content);
-    } else if (window.clipboardData) {
-        content = window.clipboardData.getData('text/html');
-        if (window.getSelection) {
-            window.getSelection().getRangeAt(0).insertNode(document.createTextNode(content));
-            $(element).html(content);
-        }
-    }
-}
-
+$(function(){
+    CKEDITOR.replace( 'question' );
+    CKEDITOR.replace( 'answer' );
+});
